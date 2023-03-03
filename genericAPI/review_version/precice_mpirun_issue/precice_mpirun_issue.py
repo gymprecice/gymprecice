6f9da84a7b3632f83d17d9829770d94cc9d5e880
Mosayeb Shams <mosayebshams10012@gmail.com>
2023-03-03 11:47:20 +0000

    Add workaround fix to minimal mpirun-precice code

--- genericAPI/review_version/precice_mpirun_issue/precice_mpirun_issue.py
+++ genericAPI/review_version/precice_mpirun_issue/precice_mpirun_issue.py
@@ -1,16 +1,19 @@
 import subprocess
+import os
  
 def launch_subprocess(cmd, run_script="", env_dir=""):
     assert cmd in ['run'], \
         "Error: invalid shell command - supported commands: 'run'"
+    
+    subproc_env = {key: variable for key, variable in os.environ.items() if "MPI" not in key}  # fix line
 
-    subproc = subprocess.Popen([f"./{run_script}"], shell=True, cwd=f"{env_dir}")
+    subproc = subprocess.Popen([f"./{run_script}"], shell=True, env=subproc_env, cwd=f"{env_dir}")
     
     return subproc
   
 if __name__ == '__main__':
     
-    import_precice = False # if True, mpirun cannot be called within python script
+    import_precice = True # if True, mpirun cannot be called within python script
 
     if import_precice:
         import precice  # this breaks mpirun
