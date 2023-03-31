f4aa24d88c3d62f976622b1f963f41e29d3b0839
Mosayeb Shams <mosayebshams10012@gmail.com>
2023-03-31 12:53:41 +0100

    Add examples folder

--- gymprecice/gymprecice/envs/openfoam/jet_cylinder_2d/fluid-openfoam/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd "${0%/*}" || exit
-. ${WM_PROJECT_DIR:?}/bin/tools/RunFunctions
-#------------------------------------------------------------------------------
-# run case
-mpirun -np $(getNumberOfProcessors) --bind-to none  pimpleFoam  -parallel > log.pimpleFoam 2>&1 &
--- gymprecice/gymprecice/utils/constants.py
+++
@@ -1,8 +0,0 @@
-
-import math
-import multiprocessing as mp
-
-LOCK = mp.Lock()
-EPSILON = 1e-6
-LOG_EPSILON = math.log(EPSILON)
-SLEEP_TIME = 0.5
